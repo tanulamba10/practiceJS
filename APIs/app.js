cbe121b3838c54455e943ee0ab76c8d4993e4b7e
Tanu Lamba <tanulamba2003@gmail.com>
2024-06-07 20:49:16 +0530

    APIs basic calls

--- APIs/app.js
+++ APIs/app.js
@@ -1,4 +1,11 @@
-let url = "https://catfact.ninja/fact";
+let url1 = "https://catfact.ninja/fact";
+let btn1 = document.querySelector("#btn1");
+let para = document.querySelector("p");
+
+
+let url2 = "https://dog.ceo/api/breeds/image/random";
+let btn2 = document.querySelector("#btn2");
+let img = document.querySelector("img");
 
 // fetch(url)
 //     .then((res) => {
@@ -13,21 +20,53 @@ let url = "https://catfact.ninja/fact";
 //     });
 
 
-async function getFacts() {
-    try {
-        let response = await fetch(url);
-        let data = await response.json();
-        console.log(data.fact);
+// async function getFacts() {
+//     try {
+//         let response = await fetch(url);
+//         let data = await response.json();
+//         console.log(data.fact);
 
-        let response1 = await fetch(url);
-        let data1 = await response1.json();
-        console.log(data1.fact);
+//         let response1 = await fetch(url);
+//         let data1 = await response1.json();
+//         console.log(data1.fact);
+
+//     } catch (err) {
+//         console.log(err);
+//     }
+
+//     console.log("ended!");
+
+// }
+
+btn1.addEventListener("click", async () => {
+    let fact = await getFact();
+    para.innerText = fact;
+});
+
+async function getFact() {
+    try {
+        let response = await axios.get(url1);
+        return response.data.fact;
 
     } catch (err) {
         console.log(err);
+        return "NO FACT FOUND!";
     }
+}
 
-    console.log("ended!");
+btn2.addEventListener("click", async () => {
+    let link = await getImage();
+    img.setAttribute("src", link);
+});
 
+async function getImage() {
+    try {
+        let response = await axios.get(url2);
+        return response.data.message;
+
+    } catch (err) {
+        console.log(err);
+        return "NO IMAGE FOUND!";
+    }
 }
 
